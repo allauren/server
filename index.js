c7f015365e25e3c86131588bb65d2dce3d9b2172
allauren <alexislaurentprof@gmail.com>
2018-06-20 20:45:35 +0200

    alleeeez

--- index.js
+++ index.js
@@ -1,22 +1,63 @@
 const express = require('express');
 const opn = require('opn');
 const mongoose = require ('mongoose');
-var parser = require('body-parser');
+const parser = require('body-parser');
+const Auth0Strategy = require('passport-auth0');
+const passport = require('passport');
+app = express();
 
 mongoose.connect('mongodb://root:2101Mana@ds016138.mlab.com:16138/datas', (err) =>{
-  if (err) { throw err; 
+  if (err) 
+  { 
+  	throw err; 
   }
-  else {
+  else 
+  {
 			console.log('Successfully connected to MongoDB')
-		}
+	}
 });
+
+var strategy = new Auth0Strategy({
+   domain:       'your-domain.auth0.com',
+   clientID:     'your-client-id',
+   clientSecret: 'your-client-secret',
+   callbackURL:  '/callback'
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+app.get('/login/google',
+  passport.authenticate('auth0', {connection: 'google-oauth2'}), function (req, res) {
+  res.redirect("/");
+});
+app.get('/callback',
+  passport.authenticate('auth0', { failureRedirect: '/login' }),
+  function(req, res) {
+    if (!req.user) {
+      throw new Error('user null');
+    }
+    res.redirect("/");
+  }
+);
+
+app.get('/login',
+  passport.authenticate('auth0', {}), function (req, res) {
+  res.redirect("/");
+});
+
 	var Schema =  mongoose.Schema;
 	var template = new Schema({ 
 			film : String,
 			author :String,
 		});
+
 	var film = mongoose.model("film", template);
-	app = express();
 	app.use(parser());
 	app.use(parser.urlencoded({extended: false}));
 	app.use(express.static('public'));
@@ -49,4 +90,4 @@ mongoose.connect('mongodb://root:2101Mana@ds016138.mlab.com:16138/datas', (err) 
 		res.type('.html');
 		res.sendFile(__dirname + '/public/index.html');
 	});
-app.listen(1080);
+app.listen(8080);
